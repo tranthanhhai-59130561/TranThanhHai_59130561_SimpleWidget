a7029bd34e6a791321404e623b5f5c51b472752c
tranthanhhai-59130561 <01674518584hai@gmail.com>
2020-04-26 19:59:33 +0700

    Main

--- app/src/main/java/vn/edu/ntu/thanhhai/tranthanhhai_59130561_simplewidget/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/thanhhai/tranthanhhai_59130561_simplewidget/MainActivity.java
@@ -3,10 +3,15 @@ package vn.edu.ntu.thanhhai.tranthanhhai_59130561_simplewidget;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.lang.invoke.MethodHandles;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,5 +35,27 @@ public class MainActivity extends AppCompatActivity {
         rdgGioiTinh = findViewById(R.id.rdgGioiTinh);
         rdgSoThich = findViewById(R.id.rdgSoThich);
         edtKhac = findViewById(R.id.edtKhac);
+    }
+
+    private void addEvents()
+    {
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hienThi();
+            }
+        });
+    }
+
+    private void hienThi()
+    {
+        switch (rdgGioiTinh.getCheckedRadioButtonId())
+        {
+            case R.id.rbNam:
+                Toast.makeText(getApplicationContext(),"Nam",Toast.LENGTH_SHORT).show();
+            case R.id.rbNu:
+                Toast.makeText(getApplicationContext(),"Nu",Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
